f11273c62f12873e5bb982b1545c05b058fd1940
jkaddu <jkaddu@yahoo.com>
2021-09-03 07:59:00 -0700

    added logic for guess the coverage

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,42 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent)
+// document.querySelector('.message').textContent = 'Correct Coverage!'
+
+// document.querySelector('.number').textContent = 4
+// document.querySelector('.score').textContent = 15
+// document.querySelector('.guess').value = 4
+
+const coverage = Math.trunc(Math.random() * 20) + 1
+console.log(coverage)
+let score = 5;
+
+document.querySelector('.number').textContent = coverage;
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value)
+    console.log(guess, typeof guess)
+
+    if(!guess) {
+        document.querySelector('.message').textContent = 'Pick a coverage.';
+    } else if(guess === coverage) {
+        if(score > 1) {
+            document.querySelector('.message').textContent = 'Correct!';
+            score++
+            document.querySelector('.score').textContent = score;
+        } else {
+             document.querySelector('.message').textContent = 'Study!';
+             document.querySelector('.score').textContent = 0;
+        }
+        
+    } else if (guess !== coverage) {
+        if(score > 1) {
+            document.querySelector('.message').textContent = 'Not the coverage!';
+            score--
+            document.querySelector('.score').textContent = score;  
+        } else {
+            document.querySelector('.message').textContent = 'Study!'
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+})
