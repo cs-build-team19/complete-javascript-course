f1c7444b8f466cd1abfd6ff32bdaf5e343276280
jkaddu <jkaddu@yahoo.com>
2021-09-07 12:18:08 -0700

    pressing escape closes modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -10,7 +10,7 @@ const openModal = function() {
     console.log('Button Clicked')
     modal.classList.remove('hidden')
     overlay.classList.remove('hidden')
-})
+}
 const closeModal = function() {
     console.log('clicked')
     modal.classList.add('hidden')
@@ -23,3 +23,11 @@ for ( let i = 0; i < btnOpenModal.length; i ++) {
 
 btnCloseModal.addEventListener('click', closeModal) 
 overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function(e) {
+    console.log(e.key)
+    if(e.key === 'Escape') {
+        // had to invoke it because the event listener is connected to the keydown event, the if block connects the Escape keydown and calls closeModal once its pressed
+        closeModal()
+    }
+})
