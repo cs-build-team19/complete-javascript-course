a29d54c6342fff64c586898d3c37db940305964e
jkaddu <jkaddu@yahoo.com>
2021-09-07 11:18:59 -0700

    added a for loop to add an event listener to each open modal btn

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,3 +5,9 @@ const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnOpenModal = document.querySelectorAll('.show-modal')
 console.log(btnOpenModal)
+
+for ( let i = 0; i < btnOpenModal.length; i ++) {
+    btnOpenModal[i].addEventListener('click', function() {
+        console.log('Button Clicked')
+    })
+}
